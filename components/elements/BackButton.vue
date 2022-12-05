4031d81f64c00a8a9c435d9b8bf38d5a64728938
andrei <andrei.vupt@gmail.com>
2022-12-05 13:55:59 -0300

    start app

---
+++ components/elements/BackButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="$router.back()" class="relative inlibe-fles mt-2 items-center justify-content-end">
+
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BackButton"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/layout/navbar.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+      <a href="https://flowbite.com" class="flex items-center">
+        <img src="../../assets/img/icon.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">App Manager - Nuxt</span>
+      </a>
+      <div class="flex items-center">
+        <a href="tel:5541251234" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">Roberto Nescau</a>
+        <a href="#" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+      </div>
+    </div>
+  </nav>
+  <nav class="bg-gray-50 dark:bg-gray-700">
+    <div class="max-w-screen-xl px-4 py-3 mx-auto md:px-6">
+      <div class="flex items-center">
+        <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+          <li>
+            <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+          </li>
+          <li>
+            <a href="#" class="text-gray-900 dark:text-white hover:underline">Usuarios</a>
+          </li>
+          <li>
+            <a href="#" class="text-gray-900 dark:text-white hover:underline">Tarefas</a>
+          </li>
+          <li>
+            <a href="#" class="text-gray-900 dark:text-white hover:underline">Produtos</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import BackButton from "~/components/elements/BackButton.vue";
+import {useState} from "#app";
+
+export default {
+  name: "navbar.vue",
+}
+</script>
+
+<style scoped>
+
+</style>
